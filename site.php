e691444edc32492329bdead238ddd4d519bc9a77
Amol <amolsbhalerao11@gmail.com>
2022-01-11 15:26:48 +0530

    basic user input practice

--- site.php
+++ site.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php
-        $name = "Amol Bhalerao";
-        $age = 5;
-        echo str_replace("Amol", "Mayur",$name);
-    ?>
+    <form action="site.php" method="get">
+        Name <input type="text" name="name">
+        Age <input type="number" name="age">
+        <input type="submit">
+    </form>
+    <br>
+    Your name is <?php echo $_GET["name"]?>
+    <br>
+    Your name is <?php echo $_GET["age"]?>
 </body>
+
 </html>
